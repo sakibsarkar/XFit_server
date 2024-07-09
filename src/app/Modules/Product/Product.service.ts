bc4c8875206a03c74d490cb6351bb2923a5ed52e
sakibsarkar <sakibsarkar707@gmail.com>
2024-07-09 09:45:13 +0600

    category filtering added

--- src/app/Modules/Product/Product.service.ts
+++ src/app/Modules/Product/Product.service.ts
@@ -9,10 +9,12 @@ const createProductService = async (payload: IProduct) => {
 };
 
 const getAllProductService = async (query: Record<string, unknown>) => {
-  const { min, max } = query;
+  const { min, max, category } = query;
+
   const minPrice = min ? parseInt(min as string) : 0;
   const maxPrice = max ? parseInt(max as string) : 0;
   const filter: Record<string, any> = {};
+  const categoryArr = category ? (category as string).trim().split(",") : [];
 
   if (minPrice && maxPrice) {
     filter.price = { $gte: minPrice, $lte: maxPrice };
@@ -20,6 +22,12 @@ const getAllProductService = async (query: Record<string, unknown>) => {
     filter.price = { $gte: minPrice };
   } else if (maxPrice) {
     filter.price = { $lte: maxPrice };
+  }
+
+  if (category && category !== " ") {
+    filter.category = {
+      $in: categoryArr.map((cat) => new RegExp(`^${cat}$`, "i")),
+    };
   }
 
   const queryModel = Product.find(filter);
